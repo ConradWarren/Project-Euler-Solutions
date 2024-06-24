8a0d0d33c425ba9e1fdcc0c252b0c112508579da
ConradWarren <conrad_warren11@outlook.com>
2024-06-24 13:03:22 -0600

    Q9 done

---
+++ problem_9/special_pythagorean_triplet.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+
+int special_pythagorean_triplet(){
+
+	const int triplet_sum = 1000;
+
+	for(int a = 1; a <= triplet_sum/3; a++){
+		for(int b = a; b <= triplet_sum/2; b++){
+			
+			int c_squared = a*a + b*b;
+			int c = std::sqrt(c_squared);
+
+			if(c * c == c_squared && a + b + c == triplet_sum){
+				return a * b * c;
+			}else if(a + b + c > triplet_sum){
+				break;
+			}
+		}
+	}
+	
+	return 0;
+}
+
+int main(void){
+	
+	std::cout<<special_pythagorean_triplet()<<std::endl;
+
+	return 0;
+}
